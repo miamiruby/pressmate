c2934511f2a801523f9d6d6c9691d1fb4e2b6cfb
Jeff Loiselle <jeff@newnewmedia.com>
2008-12-14 19:36:53 -0500

    added validation rules to configs

--- models/config.php
+++ models/config.php
@@ -2,6 +2,23 @@
 
 class Config extends AppModel {
 	
+	 var $validate = array(
+		'site_name' => array(
+			'rule' => array('custom', '/^[A-Za-z\-\' ]+$/'),
+			'message' => 'Only letters, spaces, apostrophe, and hyphens allowed'
+		),
+		'avatar_url' => array(
+			'rule' => array('url'),
+			'message' => 'Must be valid web address',
+			'allowEmpty' => true
+		),
+		'google_analytics' => array(
+			'rule' => array('custom', '/^[A-Za-z0-9\-]+$/'),
+			'message' => 'Must be valid Google Analytics key. Only letters, numbers, and hyphens allowed',
+			'allowEmpty' => true
+		)
+	);
+	
 }
 
 ?>
