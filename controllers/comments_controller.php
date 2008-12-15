5cc13e018f52554b1ee5bebd8e0680080f189b63
Jeff Loiselle <jeff@newnewmedia.com>
2008-12-14 20:05:08 -0500

    fixed bug in conditional spam protection for comments

--- controllers/comments_controller.php
+++ controllers/comments_controller.php
@@ -6,13 +6,15 @@ class CommentsController extends AppController {
 
 	function add() {
 		if (!empty($this->data)) {
-			if ($this->data['Comment']['spam_answer'] != Configure::read('Config.spam_answer')) {
-				$this->Comment->set($this->data);
-				$this->Comment->invalidate('spam_answer', 'Incorrect answer');
-				$this->Session->write('data.Comment', $this->data['Comment']);
-				$this->Session->write('validationErrors.Comment', $this->Comment->invalidFields());
-				$this->Session->setFlash(__('Failed to save comment', true), null, null, 'error');
-				$this->redirect($this->referer());
+			if (!empty($this->data['Comment']['spam_answer'])) {
+				if ($this->data['Comment']['spam_answer'] != Configure::read('Config.spam_answer')) {
+					$this->Comment->set($this->data);
+					$this->Comment->invalidate('spam_answer', 'Incorrect answer');
+					$this->Session->write('data.Comment', $this->data['Comment']);
+					$this->Session->write('validationErrors.Comment', $this->Comment->invalidFields());
+					$this->Session->setFlash(__('Failed to save comment', true), null, null, 'error');
+					$this->redirect($this->referer());
+				}
 			}
 			if ($this->Comment->save($this->data)) {
 				$this->Session->setFlash(__('Successfully saved comment', true));
