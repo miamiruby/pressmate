fb53985fbe0d291e7bc4e56d7c7eeec321e7acde
System Administrator <root@dev-speeduneed-com-2.local>
2009-01-15 19:08:01 -0500

    updating debug to support all .local domains

--- config/core.php
+++ config/core.php
@@ -38,7 +38,7 @@
  * In production mode, flash messages redirect after a time interval.
  * In development mode, you need to click the flash message to continue.
  */
-	if ($_SERVER['SERVER_NAME'] == 'localhost') {
+	if ($_SERVER['SERVER_NAME'] == 'localhost' || strstr($_SERVER['SERVER_NAME'], '.local') ) {
 		Configure::write('debug', 2);
 	} else {
 		Configure::write('debug', 0);
